09994f33f373f03f240e90351a6d06b5cb618c83
Thomas Eckerstorfer <eckerstorfer.thomas@gmail.com>
2015-08-26 11:14:11 +0200

    added installation file and server.js

---
+++ install.cmd
@@ -0,0 +1,5 @@
+npm install -g bower
+npm install peerjs
+bower install bootstrap
+bower install peerjs
+pause > nul
---
+++ server.js
@@ -0,0 +1,12 @@
+var PeerServer = require('peer').PeerServer;
+var peerserver = PeerServer({port: 9000, path: '/letstalk'});
+
+
+peerserver.on('connection', function(id) {  
+    console.log("[+] NEW PEER CONNECTION from peerjs: "+id);
+    // accept or reject code can go here if needed
+});
+peerserver.on('disconnect', function(id) {  
+    console.log("[-] END OF PEER CONNECTION from peerjs: "+id);
+    // accept or reject code can go here if needed
+});
